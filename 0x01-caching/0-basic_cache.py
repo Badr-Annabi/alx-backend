da289d296144adebf5b9e52c732b1ad20e7f0884
Badr_Annabi <badrannabi@gmail.com>
2024-06-25 19:19:43 +0100

    TASK 0

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+from base_caching import BaseCaching
+"""
+Create a class BasicCache that inherits from
+BaseCaching and is a caching system
+"""
+
+
+class BasicCache(BaseCaching):
+    """This class is a caching system"""
+
+    def put(self, key, item):
+        """Add an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+
+    def get(self, key):
+        """Get item using its key"""
+        if key is None:
+            return None
+        return self.cache_data.get(key)
