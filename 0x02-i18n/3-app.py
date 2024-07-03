6a5deff9dc57423aa0e83f866f39118ecebeb618
Badr_Annabi <badrannabi@gmail.com>
2024-07-03 01:19:08 +0100

    all

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -6,7 +6,7 @@ This file we'll Create a single
 and “Hello world” as header (<h1>)
 """
 from flask_babel import Babel
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 
 class Config:
@@ -23,10 +23,16 @@ app.url_map.strict_slashes = False
 babel = Babel(app)
 
 
+@babel.localeselector
+def get_locale() -> str:
+    """Gets locale for a web page"""
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
 @app.route('/')
 def index() -> str:
     """renders 0-index.html"""
-    return render_template('1-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == '__main__':
